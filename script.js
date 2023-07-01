d2da34844d74fe028ec120722fca833cf528e7e6
cquiogue <83323770+cquiogue@users.noreply.github.com>
2023-07-01 07:38:05 -0400

    added copy button

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function scanDocument() {
     /^\d{2}-\d{3}-\d{2}-\d{2}-[a-zA-Z]{2}$/,
     /^[a-zA-Z]{3}-[a-zA-Z]{2}-\d{3}$/,
   ];
-  var outputDiv = document.getElementById('output');
-  outputDiv.innerHTML = ''; // Clear previous results
+  var outputCode = document.getElementById('outputCode');
+  outputCode.textContent = ''; // Clear previous results
 
   var words = documentInput.split(/\s+/); // Split the document into words
 
@@ -17,19 +17,35 @@ function scanDocument() {
     for (var j = 0; j < patterns.length; j++) {
       var pattern = patterns[j];
       if (pattern.test(word)) {
-        var matchedWord = document.createElement('div');
-        matchedWord.textContent = word;
-        outputDiv.appendChild(matchedWord); // Output the match
+        outputCode.textContent += word + '\n'; // Output the match
         break; // Break the loop to avoid checking further patterns for this word
       }
     }
   }
 
-  if (outputDiv.children.length > 0) {
-    outputDiv.classList.add('show'); // Show the output container if there are matches
+  var outputDiv = document.getElementById('output');
+  if (outputCode.textContent.trim() !== '') {
+    outputDiv.style.display = 'block'; // Show the output container if there are matches
   } else {
-    outputDiv.classList.remove('show'); // Hide the output container if there are no matches
+    outputDiv.style.display = 'none'; // Hide the output container if there are no matches
   }
+}
+
+function copyOutput() {
+  var outputCode = document.getElementById('outputCode');
+  var textToCopy = outputCode.textContent;
+  navigator.clipboard.writeText(textToCopy);
+
+  var copyButton = document.getElementById('copyButton');
+  copyButton.innerHTML = '<i class="fas fa-check"></i> Copied';
+  copyButton.classList.add('copied');
+
+  setTimeout(function () {
+    copyButton.innerHTML = 'Copy';
+    setTimeout(function () {
+      copyButton.classList.remove('copied');
+    }, 300); // Delay the removal of 'copied' class to allow the transition to complete
+  }, 1200);
 }
 
 var input = document.getElementById('documentInput');
